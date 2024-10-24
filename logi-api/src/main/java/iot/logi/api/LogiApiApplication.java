eca02198d4d03151e8300dbe9d597e7ec00d16e5
Marlon Angeli <iam@marlonangeli.com.br>
2024-10-24 17:21:34 -0300

    add base spring boot project
    Signed-off-by: Marlon Angeli <iam@marlonangeli.com.br>

---
+++ logi-api/src/main/java/iot/logi/api/LogiApiApplication.java
@@ -0,0 +1,13 @@
+package iot.logi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiApiApplication.class, args);
+	}
+
+}
---
+++ logi-api/src/test/java/iot/logi/api/LogiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package iot.logi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
